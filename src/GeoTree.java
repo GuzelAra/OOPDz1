52bac47b2f6b93b9a47bbb8844e5d1955e78d435
Guzel Elanskaya <guzelarazova@yandex.ru>
2023-04-07 14:26:48 +0300

    added info

---
+++ src/GeoTree.java
@@ -0,0 +1,19 @@
+
+
+import java.util.ArrayList;
+
+public class GeoTree {
+
+    private ArrayList<Node> tree = new ArrayList<>();
+
+    public void append(Person parent, Person children, Person grandparent, Person grandsons) {
+        tree.add(new Node(parent, Relationships.PARENT, children));
+        tree.add(new Node(children, Relationships.CHILDREN, parent));
+        tree.add(new Node(grandparent, Relationships.GRANDPARENT, grandsons));
+        tree.add(new Node(grandsons, Relationships.GRANDSONS, grandparent));;
+        }
+
+    public ArrayList<Node> getTree() {
+        return tree;
+    }
+}
---
+++ src/Research.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Research {
+
+    ArrayList<Node> tree;
+
+
+    public Research(GeoTree geoTree) {
+        tree = geoTree.getTree();
+    }
+
+    public ArrayList<Person> spend(Person person, Relationships relationships) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Node t : tree) {
+            if (t.p1.getName() == person.getName() && t.re == relationships) {
+                result.add(t.p2);
+            }
+
+        }
+        return result;
+    }
+}
